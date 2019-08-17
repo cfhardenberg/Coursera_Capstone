5b5d99896be50c79224ad2e84304a550d03cb2f0
cfhardenberg <54201640+cfhardenberg@users.noreply.github.com>
2019-08-17 10:13:13 +0200

    Add files via upload

---
+++ Coursera_Capstone_ADS.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This notebook is used for Coursera's applied data science capstone
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
